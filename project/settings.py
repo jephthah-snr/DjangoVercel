6ade69d4e6696925c5df0153bfafa2ed949446ba
jephthah-snr <jsnr300@gmail.com>
2023-08-14 23:24:52 +0100

    fixed static root stuff

--- project/settings.py
+++ project/settings.py
@@ -128,7 +128,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATIC_URL = '/static/'
-# STATIC_ROOT =   os.path.join(BASE_DIR, 'static')
+STATIC_ROOT =   os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),  # Make sure 'static' directory exists in your project
 # ]
