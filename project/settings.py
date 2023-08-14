cc0a176ddf7d58813fd6fa6e9c0e11e4034b5028
jephthah-snr <jsnr300@gmail.com>
2023-08-14 23:05:47 +0100

    fixed static root stuff

--- project/settings.py
+++ project/settings.py
@@ -127,11 +127,10 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT =   os.path.join(BASE_DIR, 'static')
+# STATIC_URL = '/static/'
+# STATIC_ROOT =   os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR, 'static'),
-    '/vercel/path0/static'
+    os.path.join(BASE_DIR, 'static'),  # Make sure 'static' directory exists in your project
 ]
 
 
